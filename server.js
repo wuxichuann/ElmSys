4af4027e8956970779a2f72adbff7a37c19572a0
WKY <3418777301@qq.com>
2025-05-22 11:42:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -49,4 +49,5 @@ app.get('/api/dishes/:id', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);//wky
 });
